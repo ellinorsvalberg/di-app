d24dc5af3a8e6da7ba68438a1a748c9077abaaec
ellinorsvalberg <svalberg.ellinor@gmail.com>
2020-11-20 16:34:36 +0100

    Fixed bug in useEffect, use map instead of forEach

--- src/App.js
+++ src/App.js
@@ -2,27 +2,23 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import Collapsible from 'react-collapsible';
 
-let Parser = require('rss-parser');
-let parser = new Parser();
+const rssURL = 'https://www.di.se/rss'
+const Parser = require('rss-parser');
+const parser = new Parser();
 
 export default function App() {
   const [response, setResponse] = useState(null);
-  const articles = []
 
   useEffect(() => {
     async function fetchArticles() {
-      const feed = await parser.parseURL('https://www.di.se/rss');
+      const feed = await parser.parseURL(rssURL);
 
-      feed.items.forEach(item => {
-          item.pubDate = new Date(item.pubDate).toLocaleString();
-          articles.push(item);
-      });
-
-      setResponse(articles);
+      feed.items.map(item => item.pubDate = new Date(item.pubDate).toLocaleString());
+      setResponse(feed.items)
     }
 
     fetchArticles();
-  },);
+  }, []);
 
   const sortedArticles = response && response.sort((a, b) =>  b.pubDate - a.pubDate).slice(0, 10);
 
@@ -31,17 +27,13 @@ export default function App() {
       <header>
         <h1>Senaste nytt från Dagens Industri</h1>
         <p>Här hittar du de 10 senaste artiklarna från Di!</p>
-        {sortedArticles && sortedArticles.map(( {title, pubDate, creator, link}) => {
-          return (
-            [
-              <Collapsible className="titles" trigger={title}>
-                <p>Journalist - {creator}</p>
-                <p>Publicerad - {pubDate}</p>
-                <p><b>Läs artikeln <a href={link} target="_blank" rel="noreferrer">här</a></b></p>
-              </Collapsible>,
-            ] 
-          )
-        })}
+        {sortedArticles && sortedArticles.map(( {title, pubDate, creator, link}) =>
+          <Collapsible className="titles" trigger={title}>
+            {creator ? <p>Journalist - {creator}</p> : null}
+            <p>Publicerad - {pubDate}</p>
+            <p><b>Läs artikeln <a href={link} target="_blank" rel="noreferrer">här</a></b></p>
+          </Collapsible>
+        )}
       </header>
     </div> 
   )
