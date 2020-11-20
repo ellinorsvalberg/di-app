b6220e973a021859016880e42f6e6d3b995bc299
ellinorsvalberg <svalberg.ellinor@gmail.com>
2020-11-20 17:17:20 +0100

    removed unused className and updated check for creator

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import Collapsible from 'react-collapsible';
 
-const rssURL = 'https://www.di.se/rss'
 const Parser = require('rss-parser');
 const parser = new Parser();
+
+const RSS_URL = 'https://www.di.se/rss';
 
 export default function App() {
   const [response, setResponse] = useState(null);
 
   useEffect(() => {
     async function fetchArticles() {
-      const feed = await parser.parseURL(rssURL);
+      const feed = await parser.parseURL(RSS_URL);
 
       feed.items.map(item => item.pubDate = new Date(item.pubDate).toLocaleString());
-      setResponse(feed.items)
+      setResponse(feed.items);
     }
 
     fetchArticles();
   }, []);
 
-  const sortedArticles = response && response.sort((a, b) =>  b.pubDate - a.pubDate).slice(0, 10);
+  const sortedArticles = response && response.sort((a, b) => b.pubDate - a.pubDate).slice(0, 10);
 
   return (
     <div className="App">
@@ -28,8 +29,8 @@ export default function App() {
         <h1>Senaste nytt från Dagens Industri</h1>
         <p>Här hittar du de 10 senaste artiklarna från Di!</p>
         {sortedArticles && sortedArticles.map(( {title, pubDate, creator, link}) =>
-          <Collapsible className="titles" trigger={title}>
-            {creator ? <p>Journalist - {creator}</p> : null}
+          <Collapsible trigger={title}>
+            {creator && <p>Journalist - {creator}</p>}
             <p>Publicerad - {pubDate}</p>
             <p><b>Läs artikeln <a href={link} target="_blank" rel="noreferrer">här</a></b></p>
           </Collapsible>
