663dce173280aa9ad3a90bfc913f0bc0a5b2dabb
ellinorsvalberg <svalberg.ellinor@gmail.com>
2020-11-20 10:26:42 +0100

    fetching latest content from di RSS-feed, displaying it in collapsible components, added css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Collapsible from 'react-collapsible';
 
-function App() {
+let Parser = require('rss-parser');
+let parser = new Parser();
+
+export default function App() {
+  const [response, setResponse] = useState(null);
+  const articles = []
+
+  useEffect(() => {
+    async function fetchArticles() {
+      const feed = await parser.parseURL('https://www.di.se/rss');
+
+      feed.items.forEach(item => {
+          item.pubDate = new Date(item.pubDate).toLocaleString();
+          articles.push(item);
+      });
+
+      setResponse(articles);
+    }
+
+    fetchArticles();
+  },);
+
+  const sortedArticles = response && response.sort((a, b) =>  b.pubDate - a.pubDate).slice(0, 10);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Senaste nytt från Dagens Industri</h1>
+        <p>Här hittar du de 10 senaste artiklarna från Di!</p>
+        {sortedArticles && sortedArticles.map(( {title, pubDate, creator, link}) => {
+          return (
+            [
+              <Collapsible className="titles" trigger={title}>
+                <p>Journalist - {creator}</p>
+                <p>Publicerad - {pubDate}</p>
+                <p><b>Läs artikeln <a href={link} target="_blank" rel="noreferrer">här</a></b></p>
+              </Collapsible>,
+            ] 
+          )
+        })}
       </header>
-    </div>
-  );
+    </div> 
+  )
 }
-
-export default App;
